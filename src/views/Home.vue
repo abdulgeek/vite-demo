d4fcfd4f5975c5a19901615a35368f3b29d9c9aa
Abdul <abdul.rahman@geekseat.com.au>
2021-09-20 13:57:37 +0700

    add vue router

---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <About/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import About from './About.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    About
+  }
+}
+</script>
